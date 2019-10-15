2d0d3b01edb218dda7d3aa00abf03b5d8ab565dd
Shubhanshu <shubhanshu@sensenuts.com>
2019-10-15 15:25:50 +0530

    Kotlin Learning

---
+++ src/hello.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>)
+{
+    var name:String
+    var firstName:String = "Shubhanshu"
+    var lastname = "Singh"
+    val middleName = "Kumar"
+
+    //DataType
+     val myLong = 10L  //Long
+    val myfloat = 10F  //Float
+    val myHex = 0x0F //HexaDecima
+    val mybinary = 0xb01001 //Binarry
+
+
+
+    println(firstName+lastname)
+}
